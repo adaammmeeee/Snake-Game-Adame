1c3cffeb3f08af8619a01fff07bbac0cc0179bbd
Adame Abdelaziz <adame.abdelaziz@margo-group.net>
2026-01-30 18:00:22 +0100

    Benchmark prototype

--- GameSimulation.java
+++ GameSimulation.java
@@ -4,45 +4,8 @@ import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class GameSimulation {
-    private final ArrayList<GameLogic> logics = new ArrayList<>();
 
-
-    public GameSimulation(int logicType)
-    {
-        int[] dimensions = {10, 100, 1000, 10000, 100000};
-        int[] snakeLengths = {3, 30, 300, 3000, 30000};
-        int parameters = 3;
-        switch (logicType)
-        {
-            case 0:
-                System.out.println("Benchmarking bits implementation");
-                for(int i = 0; i< parameters; i++)
-                {
-                    logics.add(new GameLogicBit(dimensions[i], dimensions[i], snakeLengths[i]));
-                }
-                break;
-            case 1:
-                System.out.println("Benchmarking set implementation");
-                for(int i = 0; i< parameters; i++)
-                {
-                    logics.add(new GameLogicSet(dimensions[i], dimensions[i], snakeLengths[i]));
-                }
-                break;
-            case 2:
-                System.out.println("Benchmarking list implementation");
-                for(int i = 0; i< parameters; i++)
-                {
-                    logics.add(new GameLogicList(dimensions[i], dimensions[i], snakeLengths[i]));
-                }
-                break;
-
-
-        }
-    }
-
-    public void scenario() {
-        int cpt = 0;
-
+    public static void scenario(GameLogic gl) {
         long startShift;
         long startHasEaten;
         long startTestCollision;
@@ -54,48 +17,41 @@ public class GameSimulation {
         long totalShift = 0;
         long totalHasEaten = 0;
         long totalTestCollision = 0;
-
-        for(var gl : logics)
+        // Warmup part
+        for(int i=0; i<100_000; i++)
         {
-            // Warmup part
-            for(int i=0; i<100_000; i++)
+            gl.shiftSnake();
+            gl.updateHeadPosition();
+            gl.hasEaten();
+            if (gl.testCollision())
             {
-                gl.shiftSnake();
-                gl.updateHeadPosition();
-                gl.hasEaten();
-                if (gl.testCollision())
-                {
-                    System.out.println("Collision, cancel");
-                }
+                System.out.println("Collision, cancel");
             }
-            System.out.println("Start of scenario " + cpt);
-
-            // Measure time
-            for(int i=0; i<100_000; i++)
-            {
-                startShift = System.nanoTime();
-                gl.shiftSnake();
-                endShift = System.nanoTime();
-                totalShift += endShift-startShift;
-
-                gl.updateHeadPosition();
-
-                startHasEaten = System.nanoTime();
-                gl.hasEaten();
-                endHasEaten = System.nanoTime();
-                totalHasEaten += endHasEaten-startHasEaten;
-
-                startTestCollision = System.nanoTime();
-                gl.testCollision();
-                endTestCollision = System.nanoTime();
-                totalTestCollision += endTestCollision-startTestCollision;
-            }
-            System.out.println("scenario " + cpt + " duration method shiftSnake    : " + totalShift);
-            System.out.println("scenario " + cpt + " duration method hasEaten      : " + totalHasEaten);
-            System.out.println("scenario " + cpt + " duration method TestCollision : " + totalTestCollision);
-
-
-            cpt++;
         }
+        System.out.println("Start of scenario with grid size " + gl.x + "x" + gl.y + " and a length snake : " + gl.snakeLength);
+        // Measure time
+        for(int i=0; i<100_000; i++)
+        {
+            startShift = System.nanoTime();
+            gl.shiftSnake();
+            endShift = System.nanoTime();
+            totalShift += endShift-startShift;
+
+            gl.updateHeadPosition();
+
+            startHasEaten = System.nanoTime();
+            gl.hasEaten();
+            endHasEaten = System.nanoTime();
+            totalHasEaten += endHasEaten-startHasEaten;
+
+            startTestCollision = System.nanoTime();
+            gl.testCollision();
+            endTestCollision = System.nanoTime();
+            totalTestCollision += endTestCollision-startTestCollision;
+
+        }
+            System.out.println("Duration method shiftSnake    : " + totalShift);
+            System.out.println("Duration method hasEaten      : " + totalHasEaten);
+            System.out.println("Duration method TestCollision : " + totalTestCollision);
     }
 }
--- Main.java
+++ Main.java
@@ -8,32 +8,50 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-//		JFrame obj = new JFrame();
+		JFrame obj = new JFrame();
+
+		int gridWidth = 30;
+		int gridLength = 20;
+		int snakeLength = 3;
+
+		GameLogic gl1 = new GameLogicBit(gridWidth, gridLength, snakeLength);
+
+//		int[] dimensions = {10,100,1000,10000};
+//		int[] snakeLengths = {3,30,300,3000};
+//		int scenarios = 4;
+
+//		System.out.println("Benchmarking set implementation");
+//		for (int i=0; i<scenarios; i++)
+//		{
+//			GameSimulation.scenario(new GameLogicSet(dimensions[i], dimensions[i], snakeLengths[i]));
+//			System.gc();
+//		}
+
+//		System.out.println("Benchmarking list implementation");
+//		for (int i=0; i<scenarios; i++)
+//		{
+//			GameSimulation.scenario(new GameLogicList(dimensions[i], dimensions[i], snakeLengths[i]));
+//			System.gc();
+//		}
+
+//		System.out.println("Benchmarking bit implementation");
+//		for (int i=0; i<scenarios; i++)
+//		{
+//			GameSimulation.scenario(new GameLogicBit(dimensions[i], dimensions[i], snakeLengths[i]));
+//			System.gc();
+//		}
 //
-//		int gridWidth = 30;
-//		int gridLength = 20;
-//		int snakeLength = 3;
-
-//		GameLogic gl1 = new GameLogicSet(gridWidth, gridLength, snakeLength);
-
-		GameSimulation gs2 = new GameSimulation(1);
-		gs2.scenario();
-
-		GameSimulation gs3 = new GameSimulation(2);
-		gs3.scenario();
-
-		GameSimulation gs1 = new GameSimulation(0);
-		gs1.scenario();
 
 
-//		Gameplay gameplay = new Gameplay(gridWidth, gridLength, gl1);
-//
-//		obj.setBounds(0,0,gameplay.getWidth(),gameplay.getHeight());
-//		obj.setBackground(Color.DARK_GRAY);;
-//		obj.setResizable(false);
-//		obj.setVisible(true);
-//		obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//		obj.add(gameplay);
+
+		Gameplay gameplay = new Gameplay(gridWidth, gridLength, gl1);
+
+		obj.setBounds(0,0,gameplay.getWidth(),gameplay.getHeight());
+		obj.setBackground(Color.DARK_GRAY);;
+		obj.setResizable(false);
+		obj.setVisible(true);
+		obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		obj.add(gameplay);
 	}
 
 }
